71591f4d59f2b64607fb56cbad0b1f8ca949a095
asimbinahammed <asim242370@gmail.com>
2021-11-12 20:31:29 +0530

    Refactored: Parking lot owner knoing lot is full

--- src/main/java/com/bridgelab/InvoiceSummary.java
+++ src/main/java/com/bridgelab/InvoiceSummary.java
@@ -19,7 +19,8 @@ public class InvoiceSummary {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         InvoiceSummary that = (InvoiceSummary) o;
-        return numberOfRides == that.numberOfRides && Double.compare(that.totalFare, totalFare) == 0 && Double.compare(that.averageCost, averageCost) == 0;
+        return numberOfRides == that.numberOfRides && Double.compare(that.totalFare, totalFare) == 0
+                && Double.compare(that.averageCost, averageCost) == 0;
     }
 
 }
